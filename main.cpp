e96a81e221f360a2f875c247c145586344339471
Dr. Jeremy Simon Thornton <ifknot@hotmail.com>
2023-02-08 19:04:41 -0500

    popcnt

--- main.cpp
+++ main.cpp
@@ -2,4 +2,11 @@
 
 int main() {
     std::cout << "Bitboards.\n"; 
+    int32_t n{ 0 };
+    __asm {
+        mov     eax, -1
+        popcnt  ecx, eax
+        mov     n, ecx
+    }
+    std::cout << n;
 }
